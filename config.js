5eec3e3c2d45b2914e46a74f6fa58ccb3eb45c1b
Ben Fife <ammonfife@gmail.com>
2025-05-21 14:32:52 -0600

    Add Supabase and aggregator config

---
+++ config.js
@@ -0,0 +1,7 @@
+export const config = {
+  supabase_url: 'https://pmydrop.czprriuvgoqcfcdsunrs.supabase.co.net',
+  supabase_key: 'public-non-secret-key-for-feeds',
+  aggregator_wallet: '0xAGGREGATOR_WALLET'
+};
+
+export default config;
